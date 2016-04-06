8c86ddacbe24474bc2cee5d5c5b540636f041e66
Donnie Bishop <donnie.a.bishop@gmail.com>
2016-04-06 09:50:47 -0400

    Figured out shading on powermenu, linked it and compton config. Updated i3

--- utils/power_menu.sh
+++ utils/power_menu.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
  
-res=$(echo " logout| restart| shutdown" | rofi -sep "|" -dmenu -i -p "" -bw 0 -bc "#dc1900" -bg "#1e1e20" -fg "#e0d8d1" -hlbg "#1e1e20" -hlfg "#dc1900" -separator-style none -width 9 -hide-scrollbar -location 3 -padding 30 -opacity 100 -line-margin 5 -font "Hack 10")
+res=$(echo "logout|reboot|shutdown" | rofi -sep "|" -dmenu -i -p 'Power Menu: ' "" -bw 0 -bc "#dc1900" -bg "#1e1e20" -fg "#e0d8d1" -hlbg "#1e1e20" -hlfg "#dc1900" -width 9 -hide-scrollbar -eh 4 -location 3 -yoffset 23 -padding 15 -opacity 100 -font "Awesome 10, Hack 10" -auto-select) 
  
 #if [ $res = "lock" ]; then
 #    /home/khoaduccao/.config/lock.sh
 #fi
-if [ $res = " logout" ]; then
+if [ $res = "logout" ]; then
     i3-msg exit
 fi
-if [ $res = " restart" ]; then
+if [ $res = "reboot" ]; then
     systemctl reboot
 fi
-if [ $res = " shutdown" ]; then
+if [ $res = "shutdown" ]; then
     systemctl poweroff
 fi
 exit 0
