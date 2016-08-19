a682475a03ac577833bc1f277efa3680d39bbcf1
Donnie Bishop <donnie.a.bishop@gmail.com>
2016-08-19 12:54:15 -0400

    FINALLY FIXED ROFI. Linked powermenu

--- utils/power_menu.sh
+++ utils/power_menu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-res=$(echo "logout|reboot|shutdown" | rofi -sep "|" -dmenu -i -p 'Power Menu: ' "" -bw 0 -bc "#dc1900" -bg "#1e1e20" -fg "#e0d8d1" -hlbg "#1e1e20" -hlfg "#dc1900" -width 9 -hide-scrollbar -eh 4 -location 3 -yoffset 23 -padding 15 -opacity 100 -font "Awesome 10, Hack 10" -auto-select) 
+res=$(echo "logout|reboot|shutdown" | rofi -sep "|" -dmenu -i -p 'Power Menu: ' "" -width 9 -hide-scrollbar -eh 4 -location 3 -yoffset 18 -padding 12 -opacity 100 -font "Tewi 9" -auto-select) 
  
 #if [ $res = "lock" ]; then
 #    /home/khoaduccao/.config/lock.sh
