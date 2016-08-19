15eafd3110b43a1d28829d700c80d7ed209d272c
Donnie Bishop <donnie.a.bishop@gmail.com>
2016-08-19 14:02:51 -0400

    Got playstat to pause on click. I'm a genius.

--- utils/playstat.py
+++ utils/playstat.py
@@ -8,9 +8,20 @@ All it really does is put a play or pause icon based on song status.
 That's it.
 
 It's stupid, but hey, at least it looks nice.
+
+Oh yeah, I have no idea why the print statement in line 24 works. But it does
+, and keeps the status line from flickering in and out. I really don't know why
+it works, but I'm not gonna fight it either.
+
+Remove at your own risk.
 """
 
+import os
 import subprocess
+
+if os.environ['BLOCK_BUTTON'] == '1':
+    subprocess.run(['mpc', 'toggle'])
+    print(current)
 
 status = subprocess.check_output(['mpc', 'status']).decode('UTF-8')
 current = subprocess.check_output(['mpc', 'current']).decode('UTF-8').strip('\n')
