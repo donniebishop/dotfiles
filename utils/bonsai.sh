a899399a90ab0cb25f418646e0713e5940a0fa13
Donnie Bishop <donnie.a.bishop@gmail.com>
2018-01-11 09:05:23 -0500

    enju's bonsai <3

---
+++ utils/bonsai.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# made by enju <3
+echo -e "              \e[32m&&"
+echo -e "             \e[32m&&&&&"
+echo -e "           \e[32m&&&\/& &&&"
+echo -e "          \e[32m&&\e[33m|,/  |/\e[32m& &&"
+echo -e "           \e[32m&&\e[33m/   /  /_\e[32m&  &&"
+echo -e "             \e[33m\  {  |_____/_\e[32m&"
+echo -e "             \e[33m{  / /          \e[32m&&&"
+echo -e "             \e[33m.\`. \\{___\________\/_\}"
+echo -e "              \e[33m\} \}\{       \\"
+echo -e "              \e[33m}\{\{         \\____\e[32m&"
+echo -e "             \e[33m\{\}\{           \`\e[32m&\e[33m\\e[32m&&"
+echo -e "             \e[33m{{}             \e[32m&&"
+echo -e "       \e[33m, -=-~{ .-^- _"
+echo -e "             \`}"
+echo -e "              {"
