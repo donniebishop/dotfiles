435d5ee7290c5b3467fa65cf3e8a5f31e8c95791
Donnie Bishop <donnie.a.bishop@gmail.com>
2018-01-19 12:29:16 -0500

    New weather script

---
+++ utils/weather.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+import pyowm
+
+# setup, exit on failure
+try:
+    owm = pyowm.OWM('497ea9c126364ec15f0ec30762a4365d')
+    observation = owm.weather_at_place("Ashburn, US")
+    w = observation.get_weather()
+except:
+    exit(0)
+
+t = w.get_temperature('fahrenheit')
+temperature = "{}°F".format(int(t['temp']))
+#status = w.get_status()
+
+# Print status and temp
+#print(status, int(temperature))
+
+# Print just temp
+print(temperature)
